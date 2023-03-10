43b84b9e613be803876b5b355f9c5f57bb1684b1
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-03-10 20:28:19 -0300

    refactor: modularização da home, para implementação de rotas futuras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IntroComponent } from './components/intro/intro.component';
-import { KnowledgeComponent } from './components/knowledge/knowledge.component';
-import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeModule } from './modules/home/home.module';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		IntroComponent,
-		KnowledgeComponent,
-		ProjectsComponent,
-		FooterComponent,
-	],
+	declarations: [AppComponent, HeaderComponent, FooterComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
@@ -38,11 +28,12 @@ import { HeaderComponent } from './shared/header/header.component';
 		LayoutModule,
 		MatIconModule,
 		MatTooltipModule,
-		MatToolbarModule,
+
 		MatSidenavModule,
 		MatCardModule,
 		MatTabsModule,
 		HttpClientModule,
+		HomeModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IntroComponent } from './components/intro/intro.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
+@NgModule({
+	declarations: [
+		HomeComponent,
+		IntroComponent,
+		KnowledgeComponent,
+		ProjectsComponent,
+	],
+	imports: [
+		CommonModule,
+		MatCardModule,
+		MatIconModule,
+		MatTabsModule,
+		FlexLayoutModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatTooltipModule,
+	],
+})
+export class HomeModule {}
