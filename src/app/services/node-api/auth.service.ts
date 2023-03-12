aa93d27ab82c923cad621fdd755d43437b0c9072
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-03-12 14:52:09 -0300

    feat(services/node-api): serviços do projeto 'node-api' organizados em pasta única
    Todos serviços de cada projeto ficarão em 'services/<nome do projeto>'. Serviço de autenticação
    acrescentado. Formulário de login implementado e já funcionando.

---
+++ src/app/services/node-api/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface AuthServiceInput {
+	email: string;
+	password: string;
+}
+
+interface AuthServiceOutput {
+	id: string;
+	token: string;
+}
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthService {
+	constructor(private readonly request: HttpClient) {}
+
+	private readonly apiUrl = 'https://node-api-git-main-gabrielcoutz.vercel.app';
+
+	loginUser(payload: AuthServiceInput): Observable<AuthServiceOutput> {
+		return this.request.post<AuthServiceOutput>(
+			`${this.apiUrl}/login`,
+			payload,
+		);
+	}
+}
