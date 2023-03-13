4b12048ee0ff28638cd0c68c95edb6b8e4665674
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-03-13 01:58:09 -0300

    style: lint rodado

--- src/app/components/modal/created-account/created-account.component.spec.ts
+++ src/app/components/modal/created-account/created-account.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreatedAccountComponent } from './created-account.component';
 
 describe('CreatedAccountComponent', () => {
-  let component: CreatedAccountComponent;
-  let fixture: ComponentFixture<CreatedAccountComponent>;
+	let component: CreatedAccountComponent;
+	let fixture: ComponentFixture<CreatedAccountComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreatedAccountComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [CreatedAccountComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(CreatedAccountComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(CreatedAccountComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
